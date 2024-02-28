600597d3f33efc5692efc4da3e528e8a9a193b7d
AE9 <78670212+SevenDogsNTwoCats@users.noreply.github.com>
2024-02-27 18:51:12 -0600

    cambiado el manejo de los datos por  errores con la base de datos

---
+++ server/prisma/migrations/20240228003506_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Password` on the `EMPLOYEE` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "EMPLOYEE" DROP COLUMN "Password";
