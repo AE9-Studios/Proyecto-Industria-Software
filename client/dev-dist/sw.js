67de48b0f0fb6ccfc6b5807d33fe767b70d0a9a9
javierfsmontoya <javier.mnflores@gmail.com>
2024-03-05 21:42:57 -0600

    pequeña corección en el folder rrhh

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m6gag9i90u"
+    "revision": "0.gloftvo7qo8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
