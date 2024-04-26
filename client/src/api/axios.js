e535ff19a32b6477ba00ad30d50d59494c3de6b8
AE9 <78670212+SevenDogsNTwoCats@users.noreply.github.com>
2024-04-26 12:49:50 -0600

    solucionando conflicto de axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,15 +1,9 @@
 import axios from 'axios'
 
 const instance = axios.create({
-<<<<<<< HEAD
     // baseURL: "http://localhost:3000/api", //fuera del docker en local
     // baseURL: "http://localhost:8000/api", //dentro del docker, 
     baseURL: "https://classic-vision.alhanisespinal.tech/api", //para produccion,
-=======
-    baseURL: "http://localhost:3000/api", //fuera del docker en local
-    // baseURL: "http://192.168.0.10:3000/api", //dentro del docker,
-    // baseURL: "http://dominio/api", //para produccion,
->>>>>>> dev
     // baseURL: "https://hp8h5d2f-80.use2.devtunnels.ms/api", //poner el url de vscode para ver en otro lugar (añadir puerto en la terminal),
 })
 
