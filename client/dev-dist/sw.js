9cc0cdc06a3686dbbea229f368022117814f42e6
AE9 <78670212+SevenDogsNTwoCats@users.noreply.github.com>
2024-04-28 21:19:21 -0600

    pruebas

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t0r2d4e4s1g"
+    "revision": "0.ejg2lstff8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
