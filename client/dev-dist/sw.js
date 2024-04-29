8cdf15d57d29eba5cf6758fcc8c4e68ba8a4a0a1
AE9 <78670212+SevenDogsNTwoCats@users.noreply.github.com>
2024-04-28 23:35:15 -0600

    install desde dentro del container

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rsn9nq9b9u"
+    "revision": "0.ilt8sqmq5qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
